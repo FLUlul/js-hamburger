4241bb6723ce2a0f1778b1ff288227958278a589
FLU <valerio.massj@gmail.com>
2021-10-22 15:26:04 +0200

    fixed the hamburger function adding class active

--- main.js
+++ main.js
@@ -2,18 +2,13 @@
 const hamburger = document.querySelector(".hamburger-menu");
 const hambLogo = document.querySelector(".header-right > a");
 const hambClose = document.querySelector(".close");
-/* const media = window.matchMedia("(max-width: 1000px)")
-
-if (media.matches){
-    hamburger.style.display = "none"
-} */
 
 
 hambLogo.addEventListener("click", 
 
 function(){
 
-    hamburger.style.display = "block";
+    hamburger.classList.add("active");
 
 }
 
@@ -24,8 +19,9 @@ hambClose.addEventListener("click",
 
 function(){
 
-    hamburger.style.display = "none";
+    hamburger.classList.remove("active");
 
 }
 
 );
+
